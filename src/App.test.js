914ad299259a002bdbcc2497d6d53ca49ed766f8
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-21 15:44:46 -0500

    test: navigate to /bookstores when "Mis librerías" is clicked

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
 import App from './App';
 
 describe('App Component', () => {
 	test('renders appbar', () => {
-		render(<App />);
+		const history = createMemoryHistory();
+		render(
+			<Router history={history}>
+				<App />
+			</Router>
+		);
 		const appbar = screen.getByRole('banner');
 		expect(appbar).toBeInTheDocument();
 	});
--- src/components/Appbar/Appbar.test.js
+++ src/components/Appbar/Appbar.test.js
@@ -1,19 +1,38 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from "history";
+import { Router } from "react-router";
 import Appbar from '.';
 
 describe('Appbar Component', () => {
-	beforeEach(() => {
-		render(<Appbar />);
-	});
+    let history;
+    beforeEach(() => {
+        history = createMemoryHistory();
+        render(
+            <Router history={history}>
+              <Appbar/>
+            </Router>
+          );
+    })
 
 	test('render breaking bad logo', () => {
 		const logo = screen.getByRole('img');
+
 		expect(logo).toBeInTheDocument();
 		expect(logo).toHaveAttribute('alt', 'Logo breaking bad');
 	});
 
 	test('render navigation menu', () => {
 		const nav = screen.getByRole('navigation');
+
 		expect(nav).toBeInTheDocument();
 	});
+
+    test('navigate to /bookstores when "Mis librerías" is clicked', () => {
+        const bookstoresLink = screen.getByText('Mis librerías');
+        userEvent.click(bookstoresLink);
+
+        expect(history.location.pathname).toBe('/bookstores')
+
+    })
 });
