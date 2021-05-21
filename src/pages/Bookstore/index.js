2cf2afd369230ebd7c29b0d92bcec6f4e7a1e68d
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-21 18:25:14 -0500

    feat: display error message if user send form with blank input

--- src/pages/Bookstore/index.js
+++ src/pages/Bookstore/index.js
@@ -1,16 +1,57 @@
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import { Formik } from 'formik';
 import React from 'react';
 
 export default function Bookstore() {
 	return (
 		<>
-			<form name="NewBookstore">
-				<TextField variant="outlined" placeholder="Nombre de librería" />
-				<Button variant="contained" color="primary">
-					Crear
-				</Button>
-			</form>
+			<Formik
+				initialValues={{ name: '' }}
+				validate={(values) => {
+					const errors = {};
+					if (!values.name) {
+						errors.name = 'Debes ingresar un nombre';
+					}
+					return errors;
+				}}
+				onSubmit={(values, { setSubmitting }) => {
+					console.log(values);
+					setSubmitting(false);
+				}}
+			>
+				{({
+					values,
+					handleChange,
+					handleBlur,
+					handleSubmit,
+					errors,
+					isSubmitting,
+				}) => (
+					<form name="NewBookstore" onSubmit={handleSubmit}>
+						<TextField
+							variant="outlined"
+							placeholder="Nombre de librería"
+							type="text"
+							name="name"
+							id="name"
+							onChange={handleChange}
+							onBlur={handleBlur}
+							value={values.name}
+							error={!!errors.name}
+							helperText={errors.name || null}
+						/>
+						<Button
+							variant="contained"
+							color="primary"
+							disabled={isSubmitting}
+							type="submit"
+						>
+							Crear
+						</Button>
+					</form>
+				)}
+			</Formik>
 		</>
 	);
 }
