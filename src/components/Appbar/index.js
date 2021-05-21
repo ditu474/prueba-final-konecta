713601f138fd7a275e9d95226b45006a7faf7642
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-21 15:44:38 -0500

    feat: navigate to /bookstores when "Mis librerías" is clicked

--- src/components/Appbar/index.js
+++ src/components/Appbar/index.js
@@ -2,6 +2,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import breakingBadLogo from 'assets/Breaking_Bad_logo.png';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Appbar.module.scss';
 
 export default function Appbar() {
@@ -12,7 +13,9 @@ export default function Appbar() {
 					<img src={breakingBadLogo} alt="Logo breaking bad" />
 					<nav>
 						<ul>
-							<li>Mis librerias</li>
+							<li>
+								<Link to="/bookstores">Mis librerías</Link>
+							</li>
 						</ul>
 					</nav>
 				</Toolbar>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ThemeProvider from 'components/Theme';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
@@ -8,7 +9,9 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
 	<React.StrictMode>
 		<ThemeProvider>
-			<App />
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
 		</ThemeProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
