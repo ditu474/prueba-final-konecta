78dfae34f24f093f93c8a638efa84d9e5fda59fa
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-21 16:12:54 -0500

    test: navigate to / when logo is clicked

--- src/components/Appbar/Appbar.test.js
+++ src/components/Appbar/Appbar.test.js
@@ -1,21 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { createMemoryHistory } from "history";
-import { Router } from "react-router";
+import { createMemoryHistory } from 'history';
+import { MemoryRouter, Route, Router } from 'react-router';
 import Appbar from '.';
 
 describe('Appbar Component', () => {
-    let history;
-    beforeEach(() => {
-        history = createMemoryHistory();
-        render(
-            <Router history={history}>
-              <Appbar/>
-            </Router>
-          );
-    })
-
 	test('render breaking bad logo', () => {
+		render(
+			<Router history={createMemoryHistory()}>
+				<Appbar />
+			</Router>
+		);
+
 		const logo = screen.getByRole('img');
 
 		expect(logo).toBeInTheDocument();
@@ -23,16 +19,49 @@ describe('Appbar Component', () => {
 	});
 
 	test('render navigation menu', () => {
+		render(
+			<Router history={createMemoryHistory()}>
+				<Appbar />
+			</Router>
+		);
+
 		const nav = screen.getByRole('navigation');
 
 		expect(nav).toBeInTheDocument();
 	});
 
-    test('navigate to /bookstores when "Mis librerías" is clicked', () => {
-        const bookstoresLink = screen.getByText('Mis librerías');
-        userEvent.click(bookstoresLink);
+	test('navigate to /bookstores when "Mis librerías" is clicked', () => {
+		const history = createMemoryHistory();
+		render(
+			<Router history={history}>
+				<Appbar />
+			</Router>
+		);
 
-        expect(history.location.pathname).toBe('/bookstores')
+		const bookstoresLink = screen.getByText('Mis librerías');
+		userEvent.click(bookstoresLink);
 
-    })
+		expect(history.location.pathname).toBe('/bookstores');
+	});
+
+	test('navigate to / when logo is clicked', () => {
+		let testLocation;
+		render(
+			<MemoryRouter initialEntries={['/my/initial/route']}>
+				<Appbar />
+				<Route
+					path="*"
+					render={({ location }) => {
+						testLocation = location;
+						return null;
+					}}
+				/>
+			</MemoryRouter>
+		);
+
+		const logo = screen.getByAltText('Logo breaking bad');
+		userEvent.click(logo);
+
+		expect(testLocation.pathname).toBe('/');
+	});
 });
