b298c67fae850cb7c59b66652ac736ff9d324630
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-21 15:06:45 -0500

    feat: Appbar render breaking bad logo

--- src/components/Appbar/index.js
+++ src/components/Appbar/index.js
@@ -1,13 +1,15 @@
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import breakingBadLogo from 'assets/Breaking_Bad_logo.png';
 import React from 'react';
+import styles from './Appbar.module.scss';
 
 export default function Appbar() {
 	return (
 		<AppBar position="static">
 			<div className="container">
-				<Toolbar>
-					Breaking Bad Logo
+				<Toolbar className={styles.toolbar}>
+					<img src={breakingBadLogo} alt="Logo breaking bad" />
 					<nav>
 						<ul>
 							<li>Mis librerias</li>
