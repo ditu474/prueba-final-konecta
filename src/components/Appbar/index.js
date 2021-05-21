d5f3021e0b1d2502604aca4ab994a27c03d343f4
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-21 17:06:07 -0500

    feat: hide appbar on scroll

--- src/components/Appbar/index.js
+++ src/components/Appbar/index.js
@@ -1,33 +1,39 @@
 import AppBar from '@material-ui/core/AppBar';
+import Slide from '@material-ui/core/Slide';
 import Toolbar from '@material-ui/core/Toolbar';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import breakingBadLogo from 'assets/Breaking_Bad_logo.png';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Appbar.module.scss';
 
 export default function Appbar() {
+	const trigger = useScrollTrigger();
+
 	return (
-		<AppBar position="static">
-			<div className="container">
-				<Toolbar className={styles.toolbar}>
-					<NavLink to="/">
-						<img src={breakingBadLogo} alt="Logo breaking bad" />
-					</NavLink>
-					<nav>
-						<ul>
-							<li>
-								<NavLink
-									to="/bookstores"
-									activeClassName="active-route"
-									className={styles['nav-link']}
-								>
-									Mis librerías
-								</NavLink>
-							</li>
-						</ul>
-					</nav>
-				</Toolbar>
-			</div>
-		</AppBar>
+		<Slide appear={false} direction="down" in={!trigger}>
+			<AppBar>
+				<div className="container">
+					<Toolbar className={styles.toolbar}>
+						<NavLink to="/">
+							<img src={breakingBadLogo} alt="Logo breaking bad" />
+						</NavLink>
+						<nav>
+							<ul>
+								<li>
+									<NavLink
+										to="/bookstores"
+										activeClassName="active-route"
+										className={styles['nav-link']}
+									>
+										Mis librerías
+									</NavLink>
+								</li>
+							</ul>
+						</nav>
+					</Toolbar>
+				</div>
+			</AppBar>
+		</Slide>
 	);
 }
