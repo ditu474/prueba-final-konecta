153bbba2a6552ede4a2766a3ba9f1efc7d5ab9a2
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-21 23:15:13 -0500

    test: Provider return an array with the bookstores saved locally

--- src/providers/Bookstore/bookstore.provider.test.js
+++ src/providers/Bookstore/bookstore.provider.test.js
@@ -14,7 +14,7 @@ const TestComponent = () => {
 					<li key={bookstore.id} data-testid="bookstore">
 						{bookstore.name}
 						<ul>
-							{bookstore.quotes.map((quote) => (
+							{bookstore.quotes?.map((quote) => (
 								<li key={quote.id} data-testid="quote">
 									{quote.quote}
 								</li>
@@ -53,6 +53,53 @@ describe('Bookstore Provider', () => {
 		expect(mockGetSavedBookstores).toHaveBeenCalledTimes(1);
 		expect(screen.queryAllByTestId('bookstore').length).toBe(0);
 		expect(screen.queryAllByTestId('quote').length).toBe(0);
+
+		mockGetSavedBookstores.mockRestore();
+	});
+
+	test('return an array with the bookstores saved locally', () => {
+		const mockGetSavedBookstores = jest.spyOn(
+			bookstoreService,
+			'getSavedBookstores'
+		);
+		mockGetSavedBookstores.mockReturnValue([
+			{
+				id: Math.random(),
+				name: 'Mis Favoritos',
+				quotes: [
+					{
+						id: Math.random(),
+						quote: 'quote 1',
+					},
+					{
+						id: Math.random(),
+						quote: 'quote 2',
+					},
+				],
+			},
+			{
+				id: Math.random(),
+				name: 'Graciosos',
+			},
+		]);
+
+		render(
+			<BookstoreProvider>
+				<TestComponent />
+			</BookstoreProvider>
+		);
+
+		expect(screen.queryAllByTestId('bookstore').length).toBe(2);
+		expect(screen.queryAllByTestId('quote').length).toBe(2);
+		const bookstore1 = screen.queryByText('Mis Favoritos');
+		expect(bookstore1).toBeInTheDocument();
+		expect(bookstore1.children.item(0).children.item(0).textContent).toBe(
+			'quote 1'
+		);
+		expect(bookstore1.children.item(0).children.item(1).textContent).toBe(
+			'quote 2'
+		);
+		expect(screen.queryByText('Graciosos')).toBeInTheDocument();
 
 		mockGetSavedBookstores.mockRestore();
 	});
