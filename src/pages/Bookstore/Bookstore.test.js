2c56bf18016e309cc573be87bc2a37e46cbdc01d
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-21 20:34:30 -0500

    test: call addBookstore context function with correct values

--- src/pages/Bookstore/Bookstore.test.js
+++ src/pages/Bookstore/Bookstore.test.js
@@ -1,12 +1,12 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import UserEvent from '@testing-library/user-event';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import BookstoreCtx from 'context/bookstore';
 import Bookstore from '.';
 
 describe('Bookstore Page', () => {
 	test('display error message if user send form with blank input', async () => {
 		render(<Bookstore />);
 
-		UserEvent.click(screen.getByText('Crear'));
+		fireEvent.click(screen.getByText('Crear'));
 
 		await waitFor(() => {
 			expect(screen.getByText('Debes ingresar un nombre')).toBeInTheDocument();
@@ -16,16 +16,34 @@ describe('Bookstore Page', () => {
 	test('display error message if user type an invalid bookstore name', async () => {
 		render(<Bookstore />);
 
-		UserEvent.type(
-			screen.getByPlaceholderText('Nombre de librería'),
-			'1nval1d n4m3'
-		);
-		UserEvent.click(screen.getByText('Crear'));
+		fireEvent.change(screen.getByPlaceholderText('Nombre de librería'), {
+			target: { value: '1nval1d n4m3' },
+		});
+		fireEvent.click(screen.getByText('Crear'));
 
 		await waitFor(() => {
 			expect(
 				screen.getByText('El nombre sólo debe contener alfanuméricos')
 			).toBeInTheDocument();
+		});
+	});
+
+	test('call addBookstore context function with correct values', async () => {
+		const addBookstoreSpy = jest.fn();
+		render(
+			<BookstoreCtx.Provider value={{ addBookstore: addBookstoreSpy }}>
+				<Bookstore />
+			</BookstoreCtx.Provider>
+		);
+
+		fireEvent.input(screen.getByPlaceholderText('Nombre de librería'), {
+			target: { value: 'Mis favoritos' },
+		});
+		fireEvent.click(screen.getByText('Crear'));
+
+		await waitFor(() => {
+			expect(addBookstoreSpy).toHaveBeenCalledTimes(1);
+			expect(addBookstoreSpy).toHaveBeenCalledWith('Mis favoritos');
 		});
 	});
 });
