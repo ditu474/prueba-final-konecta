d89c702824ce9ba19b49f823b6cc778836411c02
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-22 10:08:31 -0500

    test: display an snackbar if add a new bookstore fails

--- src/providers/Bookstore/bookstore.provider.test.js
+++ src/providers/Bookstore/bookstore.provider.test.js
@@ -1,5 +1,6 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import BookstoreContext from 'context/bookstore';
+import SnackbarProvider from 'providers/Snackbar';
 import React from 'react';
 import { act } from 'react-dom/test-utils';
 import * as bookstoreService from 'services/bookstore';
@@ -41,32 +42,34 @@ const TestComponent = () => {
 };
 
 describe('Bookstore Provider', () => {
-	test('should not render any bookstore if service returns an empty array', async () => {
-		const mockGetSavedBookstores = jest.spyOn(
-			bookstoreService,
-			'getSavedBookstores'
-		);
-		mockGetSavedBookstores.mockReturnValue([]);
+	beforeEach(() => {
+		// No se usa por que se necesita primero hacer el mock y luego el render
+	});
+
+	afterEach(() => {
+		jest.restoreAllMocks();
+	});
+
+	test('should not render any bookstore if service returns an empty array', () => {
+		const mockGetSavedBookstores = jest
+			.spyOn(bookstoreService, 'getSavedBookstores')
+			.mockReturnValue([]);
 
 		render(
-			<BookstoreProvider>
-				<TestComponent />
-			</BookstoreProvider>
+			<SnackbarProvider>
+				<BookstoreProvider>
+					<TestComponent />
+				</BookstoreProvider>
+			</SnackbarProvider>
 		);
 
 		expect(mockGetSavedBookstores).toHaveBeenCalledTimes(1);
 		expect(screen.queryAllByTestId('bookstore').length).toBe(0);
 		expect(screen.queryAllByTestId('quote').length).toBe(0);
-
-		mockGetSavedBookstores.mockRestore();
 	});
 
 	test('return an array with the bookstores saved locally', () => {
-		const mockGetSavedBookstores = jest.spyOn(
-			bookstoreService,
-			'getSavedBookstores'
-		);
-		mockGetSavedBookstores.mockReturnValue([
+		jest.spyOn(bookstoreService, 'getSavedBookstores').mockReturnValue([
 			{
 				id: Math.random(),
 				name: 'Mis Favoritos',
@@ -88,9 +91,11 @@ describe('Bookstore Provider', () => {
 		]);
 
 		render(
-			<BookstoreProvider>
-				<TestComponent />
-			</BookstoreProvider>
+			<SnackbarProvider>
+				<BookstoreProvider>
+					<TestComponent />
+				</BookstoreProvider>
+			</SnackbarProvider>
 		);
 
 		expect(screen.queryAllByTestId('bookstore').length).toBe(2);
@@ -104,31 +109,58 @@ describe('Bookstore Provider', () => {
 			'quote 2'
 		);
 		expect(screen.queryByText('Graciosos')).toBeInTheDocument();
-
-		mockGetSavedBookstores.mockRestore();
 	});
 
-	test('add a new bookstore', () => {
+	test('add a new bookstore', async () => {
 		const mockSaveBookstores = jest.spyOn(bookstoreService, 'saveBookstores');
 		jest.spyOn(bookstoreService, 'getSavedBookstores').mockReturnValue([]);
 
 		render(
-			<BookstoreProvider>
-				<TestComponent />
-			</BookstoreProvider>
+			<SnackbarProvider>
+				<BookstoreProvider>
+					<TestComponent />
+				</BookstoreProvider>
+			</SnackbarProvider>
 		);
 
 		act(() => {
 			fireEvent.click(screen.getByText('Add Bookstore'));
 		});
 
-		expect(mockSaveBookstores).toBeCalledTimes(1);
-		expect(mockSaveBookstores).toHaveBeenCalledWith([
-			{ id: expect.any(Number), name: 'New Bookstore' },
-		]);
-		expect(screen.queryAllByTestId('bookstore').length).toBe(1);
-		expect(screen.getByText('New Bookstore')).toBeInTheDocument();
-
-		mockSaveBookstores.mockRestore();
+		await waitFor(() => {
+			expect(mockSaveBookstores).toBeCalledTimes(1);
+			expect(mockSaveBookstores).toHaveBeenCalledWith([
+				{ id: expect.any(Number), name: 'New Bookstore' },
+			]);
+			expect(screen.queryAllByTestId('bookstore').length).toBe(1);
+			expect(screen.getByText('New Bookstore')).toBeInTheDocument();
+		});
 	});
+
+	test('display an snackbar if add a new bookstore fails', async () => {
+		jest
+			.spyOn(bookstoreService, 'saveBookstores')
+			.mockReturnValue(() => 'Error');
+
+		render(
+			<SnackbarProvider>
+				<BookstoreProvider>
+					<TestComponent />
+				</BookstoreProvider>
+			</SnackbarProvider>
+		);
+
+		act(() => {
+			fireEvent.click(screen.getByText('Add Bookstore'));
+		});
+
+		await waitFor(() => {
+			expect(
+				screen.getByText('No se logró guardar la librería New Bookstore')
+			).toBeInTheDocument();
+		});
+	});
+
+	//TODO: TEST SNACKBAR AL AÑADIR Y AL FALLAR EL AÑADIR
+	//TODO: TEST CUANDO FALLE EL AÑADIR
 });
