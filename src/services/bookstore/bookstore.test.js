bff07bacebffe2c717758c1fed9f33dd83462bb8
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-21 21:36:48 -0500

    test: service return bookstores saved locally

---
+++ src/services/bookstore/bookstore.test.js
@@ -0,0 +1,45 @@
+import { getSavedBookstores } from '.';
+
+const returnValue = [
+	{
+		id: Math.random(),
+		name: 'Mis Favoritos',
+		quotes: [
+			{
+				id: Math.random(),
+				quote: 'quote 1',
+			},
+			{
+				id: Math.random(),
+				quote: 'quote 2',
+			},
+		],
+	},
+	{
+		id: Math.random(),
+		name: 'Graciosos',
+		quotes: [
+			{
+				id: Math.random(),
+				quote: 'quote 3',
+			},
+			{
+				id: Math.random(),
+				quote: 'quote 4',
+			},
+		],
+	},
+];
+
+describe('Bookstore Service', () => {
+	test('return bookstores saved locally', () => {
+		const getItemMock = jest.fn(() => JSON.stringify(returnValue));
+		Storage.prototype.getItem = getItemMock;
+
+		const response = getSavedBookstores();
+
+		expect(getItemMock).toHaveBeenCalledTimes(1);
+		expect(getItemMock).toHaveBeenCalledWith('bookstores');
+		expect(response).toEqual(returnValue);
+	});
+});
