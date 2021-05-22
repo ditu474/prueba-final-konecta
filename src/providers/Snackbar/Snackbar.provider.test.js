c4c54a05f3713628fe98ee6aa7bb40fbc312ba39
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-22 08:05:18 -0500

    test: render maximum 3 snackbars

--- src/providers/Snackbar/Snackbar.provider.test.js
+++ src/providers/Snackbar/Snackbar.provider.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { useSnackbar } from 'notistack';
 import React from 'react';
 import { act } from 'react-dom/test-utils';
@@ -13,7 +13,12 @@ const TestComponent = () => {
 		setNumberOfSnackbars((prevNum) => prevNum + 1);
 	};
 
-	return <button onClick={clickHandler}>Open Snackbar</button>;
+	return (
+		<>
+			<p data-testid="number-snackbar">{numberOfSnackbars}</p>
+			<button onClick={clickHandler}>Open Snackbar</button>
+		</>
+	);
 };
 
 describe('Snackbar Provider', () => {
@@ -29,5 +34,41 @@ describe('Snackbar Provider', () => {
 		});
 
 		expect(screen.getByText('Snackbar 1')).toBeInTheDocument();
+	});
+
+	test('render maximum 3 snackbars', async () => {
+		render(
+			<SnackbarProvider>
+				<TestComponent />
+			</SnackbarProvider>
+		);
+
+		expect(screen.getByTestId('number-snackbar').textContent).toBe('1');
+		act(() => {
+			fireEvent.click(screen.getByText('Open Snackbar'));
+		});
+		expect(screen.getByText('Snackbar 1')).toBeInTheDocument();
+
+		expect(screen.getByTestId('number-snackbar').textContent).toBe('2');
+		act(() => {
+			fireEvent.click(screen.getByText('Open Snackbar'));
+		});
+		expect(screen.getByText('Snackbar 2')).toBeInTheDocument();
+
+		expect(screen.getByTestId('number-snackbar').textContent).toBe('3');
+		act(() => {
+			fireEvent.click(screen.getByText('Open Snackbar'));
+		});
+		expect(screen.getByText('Snackbar 3')).toBeInTheDocument();
+
+		expect(screen.getByTestId('number-snackbar').textContent).toBe('4');
+		act(() => {
+			fireEvent.click(screen.getByText('Open Snackbar'));
+		});
+
+		await waitFor(() => {
+			expect(screen.getByText('Snackbar 4')).toBeInTheDocument();
+			expect(screen.queryByText('Snackbar 1')).not.toBeInTheDocument();
+		});
 	});
 });
