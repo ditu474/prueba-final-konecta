eb9838142027c3ceb5ecb5274875b3347a12fef5
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-21 22:45:01 -0500

    feat: added new props to bookstore context

--- src/context/bookstore/index.js
+++ src/context/bookstore/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const BookstoreContext = React.createContext({
+	bookstores: [],
 	addBookstore: (name) => {},
+	addQuote: (quote, bookstoreId) => {},
+	moveQuote: (quoteId, fromId, toId) => {},
 });
 
 export default BookstoreContext;
