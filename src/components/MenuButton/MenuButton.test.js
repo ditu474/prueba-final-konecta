849a9aa2047cad017eda3db93fb049a3cf3951b1
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-22 16:31:36 -0500

    feat: display the childs only when user clicks

--- src/components/MenuButton/MenuButton.test.js
+++ src/components/MenuButton/MenuButton.test.js
@@ -1,14 +1,35 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import MenuButton from '.';
 
 describe('MenuButton Component', () => {
 	test('should display child', () => {
 		render(
-			<MenuButton>
+			<MenuButton elements={[]}>
 				<div>I am the child</div>
 			</MenuButton>
 		);
 
 		expect(screen.getByText('I am the child')).toBeInTheDocument();
+	});
+
+	test('display the childs only when user clicks', () => {
+		render(
+			<MenuButton
+				elements={[
+					{ value: 10, name: 'Test 1' },
+					{ value: 20, name: 'Hi' },
+				]}
+			>
+				<button></button>
+			</MenuButton>
+		);
+
+		expect(screen.queryByText('Test 1')).not.toBeVisible();
+		expect(screen.queryByText('Hi')).not.toBeVisible();
+
+		fireEvent.click(screen.getByRole('button'));
+
+		expect(screen.queryByText('Test 1')).toBeVisible();
+		expect(screen.queryByText('Hi')).toBeVisible();
 	});
 });
--- src/components/MenuButton/index.js
+++ src/components/MenuButton/index.js
@@ -1,11 +1,41 @@
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import PropTypes from 'prop-types';
+import React from 'react';
 
-const MenuButton = ({ children }) => {
-	return <div>{children}</div>;
+const MenuButton = ({ children, elements }) => {
+	const [anchorEl, setAnchorEl] = React.useState(null);
+
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	return (
+		<div onClick={handleClick}>
+			{children}
+			<Menu
+				anchorEl={anchorEl}
+				keepMounted
+				open={Boolean(anchorEl)}
+				onClose={handleClose}
+			>
+				{elements.map((element) => (
+					<MenuItem key={element.value} onClick={handleClose}>
+						{element.name}
+					</MenuItem>
+				))}
+			</Menu>
+		</div>
+	);
 };
 
 MenuButton.propTypes = {
 	children: PropTypes.element.isRequired,
+	elements: PropTypes.array.isRequired,
 };
 
 export default MenuButton;
