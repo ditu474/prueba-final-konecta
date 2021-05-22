e3a05ddcb39bb4ae9b58a210c45a2247d47be159
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-22 08:24:59 -0500

    feat: snackbar should disappear if Dismiss is clicked

--- src/providers/Snackbar/index.js
+++ src/providers/Snackbar/index.js
@@ -1,8 +1,24 @@
+import Button from '@material-ui/core/Button';
 import { SnackbarProvider } from 'notistack';
 import PropTypes from 'prop-types';
+import React from 'react';
 
 const CustomSnackbarProvider = ({ children }) => {
-	return <SnackbarProvider maxSnack={3}>{children}</SnackbarProvider>;
+	const snackbarRef = React.createRef();
+
+	const onClickDismiss = (key) => () => {
+		snackbarRef.current.closeSnackbar(key);
+	};
+
+	return (
+		<SnackbarProvider
+			maxSnack={3}
+			ref={snackbarRef}
+			action={(key) => <Button onClick={onClickDismiss(key)}>Dismiss</Button>}
+		>
+			{children}
+		</SnackbarProvider>
+	);
 };
 
 CustomSnackbarProvider.propTypes = {
