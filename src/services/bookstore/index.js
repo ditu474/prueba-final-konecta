a869d8dd8c5624a29826e4c9942483e9ef7eb921
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-21 21:50:21 -0500

    feat: service save the bookstores locally

--- src/services/bookstore/index.js
+++ src/services/bookstore/index.js
@@ -1,7 +1,13 @@
+const BOOKSTORES_KEY = 'bookstores';
+
 export const getSavedBookstores = () => {
-	const savedBookstores = localStorage.getItem('bookstores');
+	const savedBookstores = localStorage.getItem(BOOKSTORES_KEY);
 	if (!savedBookstores) {
 		return [];
 	}
 	return JSON.parse(savedBookstores);
+};
+
+export const saveBookstores = (bookstores) => {
+	localStorage.setItem(BOOKSTORES_KEY, JSON.stringify(bookstores));
 };
