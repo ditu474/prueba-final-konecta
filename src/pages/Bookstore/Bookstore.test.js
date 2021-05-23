c09aeccfdc2ea14d4104546d2d44d0d5e7870b16
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-23 11:33:35 -0500

    fix: display error message if user type an invalid bookstore name

--- src/pages/Bookstore/Bookstore.test.js
+++ src/pages/Bookstore/Bookstore.test.js
@@ -22,9 +22,7 @@ describe('Bookstore Page', () => {
 		fireEvent.click(screen.getByText('Crear'));
 
 		await waitFor(() => {
-			expect(
-				screen.getByText('El nombre sólo debe contener alfanuméricos')
-			).toBeInTheDocument();
+			expect(screen.getByText('Solo letras')).toBeInTheDocument();
 		});
 	});
 
