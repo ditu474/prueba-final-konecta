7444519195c27386abc33c88f16b3e97dd081fe5
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-22 20:55:55 -0500

    feat: return the list of characters if fetch succeed

--- src/services/breakingBad/breakingBad.test.js
+++ src/services/breakingBad/breakingBad.test.js
@@ -28,4 +28,30 @@ describe('Breaking Bad service', () => {
 			'Error fetching Breaking Bad characters'
 		);
 	});
+
+	test('return the list of characters if fetch suceed', async () => {
+		const mockResponse = [
+			{
+				char_id: 1,
+				name: 'Walter White',
+				birthday: '09-07-1958',
+				occupation: ['High School Chemistry Teacher', 'Meth King Pin'],
+				img: 'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
+				status: 'Presumed dead',
+				nickname: 'Heisenberg',
+				appearance: [1, 2, 3, 4, 5],
+				portrayed: 'Bryan Cranston',
+				category: 'Breaking Bad',
+				better_call_saul_appearance: [],
+			},
+		];
+		jest.spyOn(global, 'fetch').mockResolvedValue({
+			ok: true,
+			json: jest.fn().mockResolvedValue(mockResponse),
+		});
+
+		const res = await getBreakingBadCharacters();
+
+		expect(res).toEqual(mockResponse);
+	});
 });
--- src/services/breakingBad/index.js
+++ src/services/breakingBad/index.js
@@ -2,10 +2,8 @@ const BASE_URL = 'https://www.breakingbadapi.com/api';
 
 export const getBreakingBadCharacters = async () => {
 	const url = `${BASE_URL}/characters`;
-	return fetch(url)
-		.then((res) => {
-			if (res.ok) return res.json();
-			throw new Error('Error fetching Breaking Bad characters');
-		})
-		.then((res) => res.results);
+	return fetch(url).then((res) => {
+		if (res.ok) return res.json();
+		throw new Error('Error fetching Breaking Bad characters');
+	});
 };
