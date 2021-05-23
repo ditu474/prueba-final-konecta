919617e2dc43376db1042f39d37124fc1cf20b7e
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-22 21:06:25 -0500

    feat: getCharactersFiltered return the list of characters if fetch succeed

--- src/services/breakingBad/breakingBad.test.js
+++ src/services/breakingBad/breakingBad.test.js
@@ -78,4 +78,30 @@ describe('Breaking Bad service', () => {
 			getCharactersFiltered({ limit: 10, offset: 5 })
 		).rejects.toThrow('Error fetching Breaking Bad characters');
 	});
+
+	test('getCharactersFiltered return the list of characters if fetch succeed', async () => {
+		const mockResponse = [
+			{
+				char_id: 1,
+				name: 'Walter White',
+				birthday: '09-07-1958',
+				occupation: ['High School Chemistry Teacher', 'Meth King Pin'],
+				img: 'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
+				status: 'Presumed dead',
+				nickname: 'Heisenberg',
+				appearance: [1, 2, 3, 4, 5],
+				portrayed: 'Bryan Cranston',
+				category: 'Breaking Bad',
+				better_call_saul_appearance: [],
+			},
+		];
+		jest.spyOn(global, 'fetch').mockResolvedValue({
+			ok: true,
+			json: jest.fn().mockResolvedValue(mockResponse),
+		});
+
+		const res = await getCharactersFiltered({ limit: 1, offset: 0 });
+
+		expect(res).toEqual(mockResponse);
+	});
 });
