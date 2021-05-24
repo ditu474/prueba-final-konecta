200aeb23c67c88de75145cbbe5fa80fe6091641d
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-23 21:37:24 -0500

    chore: moved routes out of components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import Toolbar from '@material-ui/core/Toolbar';
 import Appbar from 'components/Appbar';
 import LoadingSpinner from 'components/LoadingSpinner';
-import Routes from 'components/Routes';
 import ScrollTop from 'components/ScrollTop';
 import React from 'react';
+import Routes from 'Routes';
 
 function App() {
 	return (
