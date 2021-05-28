4a55a3f9b146ff302519da7b097c8f165e0d49fd
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-05-28 10:36:41 -0500

    test: addBookstore dispatch the correct type and payload

--- src/store/slices/bookstore/bookstore.test.js
+++ src/store/slices/bookstore/bookstore.test.js
@@ -1,11 +1,30 @@
-import bookstoreSlice from '.';
+import createMockStore from 'redux-mock-store';
+import bookstoreSlice, { addBookstore } from '.';
 
 describe('Bookstore Slice', () => {
-	test('initial state', () => {
-		const result = bookstoreSlice.reducer(undefined, () => {});
-		expect(result).toEqual({
-			bookstores: [],
-			error: null,
+	const mockStore = createMockStore();
+
+	describe('given empty action and undefined state', () => {
+		it('should return the initial state', () => {
+			const result = bookstoreSlice.reducer(undefined, () => {});
+			expect(result).toEqual({
+				bookstores: [],
+				error: null,
+			});
+		});
+	});
+
+	describe('on addBookstore action', () => {
+		it('should dispatch the correct type and payload', () => {
+			const store = mockStore();
+			const newBookstore = { id: 10, name: 'Hello' };
+			store.dispatch(addBookstore(newBookstore));
+
+			const actions = store.getActions();
+			const expectedActions = [
+				{ type: 'bookstore/addBookstore', payload: newBookstore },
+			];
+			expect(actions).toEqual(expectedActions);
 		});
 	});
 });
